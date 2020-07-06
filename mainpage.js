b35e2fa76a1d5890b85f68c1452b256f76272db6
Jody Dahmer <jodydahmer@gmail.com>
2020-07-06 19:57:43 -0400

    First Time Javascript!

---
+++ mainpage.js
@@ -0,0 +1,6 @@
+
+let userCommute=prompt ("What is your average commute time?");
+
+let Commute = prompt ("What is your commute home?");
+
+let totalTime= print ("You spend" + usertime + "hours a year driving your car to/from work");
