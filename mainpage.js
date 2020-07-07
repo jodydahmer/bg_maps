f8ac1a1844833465c408e018d94073d1dc694899
Jody Dahmer <jodydahmer@gmail.com>
2020-07-07 17:39:12 -0400

    Made new nav bar with Flexbox!

--- mainpage.js
+++ mainpage.js
@@ -4,3 +4,4 @@ let userCommute=prompt ("What is your average commute time?");
 let Commute = prompt ("What is your commute home?");
 
 let totalTime= print ("You spend" + usertime + "hours a year driving your car to/from work");
+
