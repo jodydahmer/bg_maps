7970797b4abe589ab967a5340dee105a140a0e45
Jody Dahmer <jodydahmer@gmail.com>
2020-07-22 17:55:02 -0400

    Added better hamburger menu with img tags

--- commuteCalc.js
+++ commuteCalc.js
@@ -1,3 +1,7 @@
 commuteCalc.js
 
 var commuteCalc = prompt('What is your commute to work? )
+
+document.write(<p>You spend $(commute) hours a week driving to/from work! </p>);
+document.write(<h3>You spend $(commute) hours a month driving to/from work! </h3>);
+document.write(<h1>You spend $(commute) hours a year driving to/from work! </h1>)
