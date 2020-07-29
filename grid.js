96ae5e741387687314cbe007700d2423d4ab61f1
Jody Dahmer <jodydahmer@gmail.com>
2020-07-29 17:58:59 -0400

    Tried to get JS to write to HTML

--- grid.js
+++ grid.js
@@ -69,6 +69,8 @@ console.log( "Rounded down, you drive about" + " " + daysPerYear + " " + standar
 
   print("You said you drove" + carCommuteTime + " " + standardTimeIncrements[1][1]);
   console.log("You said you drove" + " " + carCommuteTime + " " + standardTimeIncrements[1][1]);
+
+  document.getElementById('commute').innerHTML = (" You drive" + " " + carCommuteDay + " " + standardTimeIncrements[1][1] +" " +  "round-trip to work!");
   
   // function printCommute (commute) {
     //     var minutesCommute = '<ol> ';
