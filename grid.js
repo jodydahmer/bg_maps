316419fb00acc1c9550e2749afbbaa07865ed92d
Jody Dahmer <jodydahmer@gmail.com>
2020-07-29 17:04:46 -0400

    Fleshed out JS and added a r r a y s

--- grid.js
+++ grid.js
@@ -1,4 +1,6 @@
- let standardTimeIncrements = [
+//Made a two-dimensional array! 
+
+let standardTimeIncrements = [
                  ['60', 'seconds'],
                  ['60','minutes'],
                  ['24','hours'],
@@ -14,23 +16,24 @@ let carCommuteTime = parseInt(input);
  //You have two commutes a day, one to work and one going home
     
 let carCommuteDay = carCommuteTime * 2;
-console.log(carCommuteDay);
+console.log(" You drive" + " " + carCommuteDay + " " + standardTimeIncrements[1][1] +" " +  "round-trip to work!");
 
 // You don't work weekends, so you need to multiply by 5
 let carCommuteWeek = carCommuteDay * 5;
-console.log(carCommuteWeek);
+console.log(" You drive" + " " + carCommuteWeek + " " + standardTimeIncrements[1][1] + " every week!");
+console.log(" That's" + " " + carCommuteWeek/60 + " " + standardTimeIncrements[2][1] + "!");
 
-//Hopefully you have vacation days, so only working 50 weeks a year. 
+//Hopefully you have vacation and sick days, so only working 50 weeks a year. 
 
 let carCommuteYear = carCommuteWeek * 50;    
-console.log(carCommuteYear);
+console.log(" You drive" + " " + carCommuteYear + " " + standardTimeIncrements[1][1] + " every year!");
+console.log(" That's" + " " + carCommuteYear/60/24 + " " + standardTimeIncrements[3][1] + "!");
 
 let hoursPerWeek = carCommuteWeek/60;
-console.log( Math.ceil(hoursPerWeek));
+console.log( "Rounded up, you drive almost" + " " + Math.ceil(hoursPerWeek) + " " + standardTimeIncrements[2][1]);
 let daysPerYear = Math.floor(carCommuteYear/60/24);
+console.log( "Rounded down, you drive about" + " " + daysPerYear + " " + standardTimeIncrements[3][1]);
 
-
-console.log(daysPerYear);
 
   function print(message) {
       document.write(message);
@@ -39,7 +42,8 @@ console.log(daysPerYear);
 
   print("You said you drove" + carCommuteTime + " " + standardTimeIncrements[1][1]);
   console.log("You said you drove" + " " + carCommuteTime + " " + standardTimeIncrements[1][1])
-    // function printCommute (commute) {
+  
+  // function printCommute (commute) {
     //     var minutesCommute = '<ol> ';
     //     for (var i=0 ; i < commute.length; i += 1) {
     //         minutesCommute += '<li>' + '<p> You drive for ' + carCommuteWeek[i] + 'every' +  + '</p>' + '</li>';
